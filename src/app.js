9b9764a0b0a4cb99b009a84ab39fb0c516f45350
lili14gon <liligh14@gmail.com>
2025-09-29 20:24:00 +0200

    avance de actividad

--- src/app.js
+++ src/app.js
@@ -1,11 +1,44 @@
 import "bootstrap";
 import "./style.css";
-
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+const btnActualizar= document.getElementById("btnActualizar");
+const valorCentral = ['A',2,3,4,5,6,7,8,9,'J','Q','K'];
+const simbolo= [1,2,3,4];
+const valorCentralAleatorioHtml= document.getElementById("valorCentro");
+const simboloHtmlS= document.getElementById("imagenSimboloS");
+const simboloHtmlI= document.getElementById("imagenSimboloI");
+
+const action =(e) =>{
+  const valorCentralAleatorio = Math.floor(Math.random() * valorCentral.length);
+
+    valorCentralAleatorioHtml.innerHTML= valorCentral[valorCentralAleatorio];
+    const simboloAleatorio = Math.floor(Math.random() * simbolo.length);
+    
+    if(simbolo[simboloAleatorio]==1){
+      simboloHtmlS.innerHTML= "♦";
+      simboloHtmlS.style.color="red"
+      simboloHtmlI.innerHTML= "♦";
+      simboloHtmlI.style.color="red"
+    } else if (simbolo[simboloAleatorio]==2){
+      simboloHtmlS.innerHTML= "♥";
+      simboloHtmlS.style.color="red"
+      simboloHtmlI.innerHTML= "♥";
+      simboloHtmlI.style.color="red"
+    } else if (simbolo[simboloAleatorio]==3){
+      simboloHtmlS.innerHTML= "♠";
+      simboloHtmlS.style.color="black";
+      simboloHtmlI.innerHTML= "♠";
+      simboloHtmlI.style.color="black";
+    } else{
+      simboloHtmlS.innerHTML= "♣";
+      simboloHtmlS.style.color="black";
+      simboloHtmlI.innerHTML= "♣";
+      simboloHtmlI.style.color="black";
+    } 
+}
+
+window.onload = function() {action()}
+btnActualizar.addEventListener('click',action )
